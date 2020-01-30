b9f3f1714977a5a18b8d32b5ea8959ff8c8e5ba4
2Jak <60384209+2Jak@users.noreply.github.com>
2020-01-30 14:58:20 +0200

    removing dummy data from app.js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
-import moment from 'moment';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: moment().valueOf() }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
 const jsx = (
   <Provider store={store}>
