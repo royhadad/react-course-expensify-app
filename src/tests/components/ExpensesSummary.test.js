be18af7144142877f0ad36723a09c18342d40b0a
Bob <bob@example.com>
2020-02-03 12:12:33 +0200

    changing the expenseSummary.test headers

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -3,12 +3,12 @@ import { shallow } from "enzyme";
 import expenses from "../fixtures/expenses";
 import { ExpensesSummary } from '../../components/ExpensesSummary';
 
-test('should match snapshot with all 3 expenses', () => {
+test('should correctly render ExpenseSumary with all 3 expenses', () => {
     const wrapper = shallow(<ExpensesSummary expenseCount={expenses.length} expensesTotal={114195} />);
     expect(wrapper).toMatchSnapshot();
 });
 
-test('should match snapshot with 1 expense', () => {
+test('should correctly render ExpenseSumary with 1 expense', () => {
     const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={expenses[0].amount} />);
     expect(wrapper).toMatchSnapshot();
 });
