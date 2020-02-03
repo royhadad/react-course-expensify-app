90e7b646a88c4e905342616a5070691c664ab42f
Bob <bob@example.com>
2020-02-03 12:09:10 +0200

    refactoring ExpensesSummary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,10 +6,12 @@ import numeral from 'numeral';
 
 export class ExpensesSummary extends React.Component {
     render() {
+        const expenseWord = this.props.expenseCount === 1 ? 'expense' : 'expenses';
+        const formattedExpensesTotal = numeral(this.props.expensesTotal / 100).format('$0,0.00');
         return (
             <div>
                 <p>
-                    viewing {this.props.expenseCount} expense{this.props.expenseCount === 1 || 's'} totalling {numeral(this.props.expensesTotal / 100).format('$0,0.00')}
+                    viewing {this.props.expenseCount} {expenseWord} totalling {formattedExpensesTotal}
                 </p>
             </div>
         );
