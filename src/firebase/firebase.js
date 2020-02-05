44524bae170192b29183f8e6d0c006fce6f0c4fd
Bob <bob@example.com>
2020-02-05 11:30:35 +0200

    setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,32 @@
+import expenses from '../tests/fixtures/expenses';
+import * as firebase from 'firebase';
+var config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain:process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId:process.env.FIREBASE_APP_ID,
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export {firebase, database as default};
+
+// database.ref('expenses')
+//     .on('value', (snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+//         console.log(expenses);
+//     });
+// database.ref('expenses')
+//     .on('child_changed', (childSnapshot) => {
+//         console.log(childSnapshot.key, childSnapshot.val());
+//     });
