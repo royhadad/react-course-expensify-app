b060cba7b5b1b981cc9a80a09b0d8fe5b22745b3
MrShmalo <dorevgi4@gmail.com>
2020-02-06 16:59:45 +0200

    adding styles up to 173 8:50

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,16 +3,22 @@ import { connect } from 'react-redux';
 import getExpensesTotal from '../selectors/expenses-total';
 import getVisibleExpenses from '../selectors/expenses';
 import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 
 export class ExpensesSummary extends React.Component {
     render() {
         const expenseWord = this.props.expenseCount === 1 ? 'expense' : 'expenses';
         const formattedExpensesTotal = numeral(this.props.expensesTotal / 100).format('$0,0.00');
         return (
-            <div>
-                <h1>
-                    viewing {this.props.expenseCount} {expenseWord} totalling {formattedExpensesTotal}
-                </h1>
+            <div className="page-header">
+                <div className="content-container">
+                    <h1 className="page-header__title">
+                        viewing <span>{this.props.expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span>
+                    </h1>
+                    <div className="page-header__actions">
+                        <Link className="button" to="/create">Add Expense</Link>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,12 @@ import { startLogin } from '../actions/auth';
 export class LoginPage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-                <button onClick={this.props.startLogin}>Login</button>
+            <div className="box-layout">
+                <div className="box-layout__box">
+                    <h1 className="box-layout__title">Expensify</h1>
+                    <p>It's time to get your expenses under control.</p>
+                    <button className="button" onClick={this.props.startLogin}>Login with Google</button>
+                </div>
             </div>
         );
     }
