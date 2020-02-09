e88b47f8344c6d2f610c8108989e1d232cfabf4a
MrShmalo <dorevgi4@gmail.com>
2020-02-09 13:59:38 +0200

    add expensify styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,23 +4,32 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-        props.expenses.map((expense) => {
-          return <ExpenseListItem key={expense.id} {...expense} />;
-        })
-      )
-    }
-  </div>
+    <div className="content-container">
+        <div className="expense-list">
+            <div className="expense-list-header">
+                <div className="show-for-mobile">Expenses</div>
+                <div className="show-for-desktop">Expenses</div>
+                <div className="show-for-desktop">Amount</div>
+            </div>
+            <div className="list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <span className="list-item--message">No expenses</span>
+                ) : (
+                        props.expenses.map((expense) => {
+                            return <ExpenseListItem key={expense.id} {...expense} />;
+                        })
+                    )
+            }
+            </div>
+        </div>
+    </div>
 );
 
 const mapStateToProps = (state) => {
-  return {
-    expenses: selectExpenses(state.expenses, state.filters)
-  };
+    return {
+        expenses: selectExpenses(state.expenses, state.filters)
+    };
 };
 
 export default connect(mapStateToProps)(ExpenseList);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if (process.env.NODE_ENV === 'test') {
 } else if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config({path: '.env.development'});
+    require('dotenv').config({ path: '.env.development' });
 }
 
 module.exports = (env) => {
@@ -13,7 +13,7 @@ module.exports = (env) => {
     const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return {
-        entry: './src/app.js',
+        entry: ['babel-polyfill', './src/app.js'],
         output: {
             path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
@@ -46,13 +46,13 @@ module.exports = (env) => {
         plugins: [
             CSSExtract,
             new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY':JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN':JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL':JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID':JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET':JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID':JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-                'process.env.FIREBASE_APP_ID':JSON.stringify(process.env.FIREBASE_APP_ID)
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID)
             })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
